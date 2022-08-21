6445ebe7c34bbf69c58a77ba1b18fd90ce756429
amit singh <amit@asparia.com>
2022-08-21 19:48:37 +0530

    livechat changes

--- src/layouts/liveChat/liveChat.js
+++ src/layouts/liveChat/liveChat.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import NavBar from '../../components/navBar/navBar';
 import { historyState } from '../../constant/global';
 import { userService } from '../../services';
+import './liveChat.css';
 
 const LiveChat = () => {
+    const [state, setState] = useState({
+
+    });
+    const [activeUsers, setActiveUsers] = useState([
+      {
+        userId: '1',
+        userName: 'Amit',
+      }
+    ]);
+
     useEffect(() => {
       const user = userService.getUser();
 
@@ -12,8 +23,20 @@ const LiveChat = () => {
         historyState.history.push('/admin');
       }
     });
+
+    const activeUsersList = activeUsers.map(user => {
+      return (
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            
+            <Card.Title>{user.userName}</Card.Title>
+          </Card.Body>
+        </Card>
+      )
+    });
+
     return (
-      <Container>
+      <Container fluid>
         <Row>
             <Col className='text-center'>
                 <h1>Live Chat</h1>
@@ -23,6 +46,19 @@ const LiveChat = () => {
             <Col>
                 <NavBar/>
             </Col>
+        </Row>
+
+        <Row>
+          <Col lg={4}>
+            <div className='left-users-panel'>
+              {activeUsersList}
+            </div>
+          </Col>
+
+          <Col lg={8}>
+            <div className='right-chat-window'>
+            </div>
+          </Col>
         </Row>
     </Container>
     )
